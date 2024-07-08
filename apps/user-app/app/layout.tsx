7b7dede2cca9009425a8a3f58cc7a1366e61df25
deauthe <deauth@ANURAGs-MacBook-Air.local>
2024-07-08 15:00:53 +0530

    missing updates

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "../providers/providers";
+import { AppbarClient } from "@/components/AppBarClient";
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
 	variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<Providers>
+				<AppbarClient />
 				<body className={`${geistSans.variable} ${geistMono.variable}`}>
 					{children}
 				</body>
